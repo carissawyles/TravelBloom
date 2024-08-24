aedbfe1974a6d983451104e3569b3796c78f4518
carissawyles <cwyles725@gmail.com>
2024-08-24 16:46:23 -0400

    added js to check contact email is in valid form, and added thank you message on submit

--- contact.js
+++ contact.js
@@ -1,7 +1,15 @@
+const form = document.getElementById("contactForm");
+const email = document.getElementById("email");
 
-function checkdata(){
-    
-     //If all is well show alert message and return true.
-	  alert('Thank you for contacting us!')
-      return true;
-}
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Emails must be in the form name@email.com");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    form.innerHTML = `<div class="thanks"><h2>Thank you for contacting us!</h2></div>`
+});
